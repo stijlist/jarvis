453038980430fe80d60fbc815a30fbc3a6d60543
Sophia Davis <scdgrapefruit@gmail.com>
2014-07-30 17:41:31 -0400

    Parses command, day, room

--- lib/listener.rb
+++ lib/listener.rb
@@ -1,5 +1,22 @@
 class Listener
+
+  COMMANDS = ["reserve"] # make set?
+  ROOMS = ["Hopper"]
+  DAYS = ["Tuesday"]
+  
+
   def self.listen(command)
-    {command: command.match(/^(\w+)/)[0].to_sym}
+    parsed_input = command.split
+    
+    puts parsed_input
+    
+    # & -- set intersection
+    parsed_command = (COMMANDS & parsed_input)[0].to_sym
+    parsed_room = (ROOMS & parsed_input)[0]
+    parsed_day = (DAYS & parsed_input)[0]
+    
+    {command: parsed_command, 
+     room: parsed_room,
+     day: parsed_day}
   end
 end
--- spec/listener_spec.rb
+++ spec/listener_spec.rb
@@ -6,4 +6,16 @@ describe Listener do
   it 'understands the reserve command' do
     expect(result[:command]).to eq(:reserve)
   end
+  
+  it 'parses room names' do
+    expect(result[:room]).to eq('Hopper')
+  end
+  
+  it 'parses days' do
+    expect(result[:day]).to eq('Tuesday')
+  end
+  
+  it 'parses time' do
+    expect(result[:time]).to eq(1700)
+  end
 end
