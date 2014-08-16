d4102d35b1312c73f9409226ecdcfc89d477ae13
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-08-16 16:15:33 -0400

    Change from 4-space to 2-space indentation

--- jarvis.rb
+++ jarvis.rb
@@ -3,45 +3,46 @@ require_relative './lib/calendar.rb'
 require 'zulip'
 
 class Jarvis
-    config = {greeting: 'Jarvis is online.',
-              streams: File.read('./streams_filtered').lines.map(&:chomp)}
+  config = {greeting: 'Jarvis is online.',
+            streams: File.read('./streams_filtered').lines.map(&:chomp)}
 
-    cal = Calendar.new
-    zulip = Zulip::Client.new do |config|
-        config.email_address = ENV.fetch( 'ZULIP_JARVIS_EMAIL' )
-        config.api_key = ENV.fetch( 'ZULIP_JARVIS_API_KEY' )
-    end
+  cal = Calendar.new
+  zulip = Zulip::Client.new do |config|
+    config.email_address = ENV.fetch( 'ZULIP_JARVIS_EMAIL' )
+    config.api_key = ENV.fetch( 'ZULIP_JARVIS_API_KEY' )
+  end
 
-    # config.fetch(:streams).each do |stream|
-    #     zulip.subscribe(stream)
-    # end
+  # config.fetch(:streams).each do |stream|
+#     zulip.subscribe(stream)
+  # end
 
-    zulip.send_message('jarvis-testing', config['greeting'], 'test-bot')
-    
-    zulip.stream_messages do |message|
-        stream = message.stream
-        if message.content.match(/(\@jarvis|\@\*\*jarvis\*\*)/)
-            begin
-                parsed_message = Listener.listen(message.content, message.sender_full_name)
-                
-                request_start = parsed_message[:date]
-                request_end = request_start + Rational(1, 24) # add one hour
-                room = parsed_message[:room].capitalize
-                description = parsed_message[:description]
-             
-                bookable = cal.bookable_for?(request_start, request_end, room)
-                if bookable
-                    event = cal.add(request_start, request_end, room, description)
-                    link = event['htmlLink']
-                    response = "Booked #{room} on #{request_start.strftime("%A, %B %d from %H:%M")} to #{request_end.strftime("%H:%M")} for #{description}: [#{link}](#{link})"
-                else
-                    response = "#{room} is busy #{request_start.strftime("at %H:%M on %A, %B %d")}."
-                end
-            rescue Listener::FailedToParse => e
-                response = e.message
-            end
-            zulip.send_message(message.subject, response, stream)
-        end
-    end
+  zulip.send_message('jarvis-testing', config['greeting'], 'test-bot')
+  
+  zulip.stream_messages do |message|
+    stream = message.stream
+    if message.content.match(/(\@jarvis|\@\*\*jarvis\*\*)/)
+      begin
+        parsed_message = Listener.listen(message.content, message.sender_full_name)
         
+        request_start = parsed_message[:date]
+        request_end = request_start + Rational(1, 24) # add one hour
+        room = parsed_message[:room].capitalize
+        description = parsed_message[:description]
+     
+        bookable = cal.bookable_for?(request_start, request_end, room)
+        if bookable
+          event = cal.add(request_start, request_end, room, description)
+          link = event['htmlLink']
+          response = "Booked #{room} on #{request_start.strftime("%A, %B %d from %H:%M")} to #{request_end.strftime("%H:%M")} for #{description}: [#{link}](#{link})"
+        else
+          response = "#{room} is busy #{request_start.strftime("at %H:%M on %A, %B %d")}."
+        end
+      rescue Listener::FailedToParse => e
+        response = e.message
+      end
+
+      zulip.send_message(message.subject, response, stream)
+    end
+  end
+      
 end
--- lib/calendar.rb
+++ lib/calendar.rb
@@ -10,76 +10,76 @@ require 'digest/sha2'
 require 'openssl'
 
 class Authentication
-    JARVIS_SECRET_PATH = "../Jarvis-8746cc8b4449.json"
-    JARVIS_GOOGLE_SCOPE = "https://www.googleapis.com/auth/calendar"
-    GOOGLE_AUD_URL = "https://accounts.google.com/o/oauth2/token"
-    GOOGLE_GRANT_TYPE = "urn:ietf:params:oauth:grant-type:jwt-bearer"
-    
-    attr_accessor :token, :response_dict
+  JARVIS_SECRET_PATH = "../Jarvis-8746cc8b4449.json"
+  JARVIS_GOOGLE_SCOPE = "https://www.googleapis.com/auth/calendar"
+  GOOGLE_AUD_URL = "https://accounts.google.com/o/oauth2/token"
+  GOOGLE_GRANT_TYPE = "urn:ietf:params:oauth:grant-type:jwt-bearer"
+  
+  attr_accessor :token, :response_dict
 
-    def initialize
-        uri = URI(GOOGLE_AUD_URL)
-        grant_type = GOOGLE_GRANT_TYPE
-        response_body = Net::HTTP.post_form(uri, {"grant_type" => grant_type,
-                                                      "assertion" => make_jwt}).body
-        @response_dict = JSON.parse(response_body)
-        @token = @response_dict["access_token"]
-    end
+  def initialize
+    uri = URI(GOOGLE_AUD_URL)
+    grant_type = GOOGLE_GRANT_TYPE
+    response_body = Net::HTTP.post_form(uri, {"grant_type" => grant_type,
+                                                  "assertion" => make_jwt}).body
+    @response_dict = JSON.parse(response_body)
+    @token = @response_dict["access_token"]
+  end
 
 # Creates a JSON web token according to Google API specifications
 #
 # Returns the jwt   
-    def make_jwt
-        iss = ENV.fetch('GOOGLE_JARVIS_CLIENT_EMAIL') 
-        iat = DateTime.now.to_time.strftime('%s').to_i
-        exp = iat + 60 * 60 # expires 1 hour (60*60) from now
-        jwt_claim = {
-           "iss" => iss,
-           "scope" => JARVIS_GOOGLE_SCOPE,
-           "aud" => GOOGLE_AUD_URL,
-           "exp" => exp,
-           "iat" => iat
-        }
+  def make_jwt
+    iss = ENV.fetch('GOOGLE_JARVIS_CLIENT_EMAIL') 
+    iat = DateTime.now.to_time.strftime('%s').to_i
+    exp = iat + 60 * 60 # expires 1 hour (60*60) from now
+    jwt_claim = {
+      "iss" => iss,
+      "scope" => JARVIS_GOOGLE_SCOPE,
+      "aud" => GOOGLE_AUD_URL,
+      "exp" => exp,
+      "iat" => iat
+    }
 
-        ssl_key = OpenSSL::PKey::RSA.new ENV.fetch('GOOGLE_JARVIS_PRIVATE_KEY') 
-        JWT.encode(jwt_claim, ssl_key, "RS256")
-    end
+    ssl_key = OpenSSL::PKey::RSA.new ENV.fetch('GOOGLE_JARVIS_PRIVATE_KEY') 
+    JWT.encode(jwt_claim, ssl_key, "RS256")
+  end
 end
 
 class Calendar
-    JARVIS_TEST_CALENDAR_ID = "27oim6lpetk763ipds6au4cmgc@group.calendar.google.com"
-    API_BASE = "https://www.googleapis.com/calendar/v3/calendars/#{JARVIS_TEST_CALENDAR_ID}"
-        # todo: factor out id
-    attr_accessor :id
-    
-    def initialize(id = JARVIS_TEST_CALENDAR_ID) 
-        @auth_token = Authentication.new.token
-        @id = id
-    end
+  JARVIS_TEST_CALENDAR_ID = "27oim6lpetk763ipds6au4cmgc@group.calendar.google.com"
+  API_BASE = "https://www.googleapis.com/calendar/v3/calendars/#{JARVIS_TEST_CALENDAR_ID}"
+      # todo: factor out id
+  attr_accessor :id
+  
+  def initialize(id = JARVIS_TEST_CALENDAR_ID) 
+    @auth_token = Authentication.new.token
+    @id = id
+  end
 
 # Gets a list of events scheduled on a given calendar
 #
 # Returns a list of hashes, each representing one event 
-    def events
-        #use http to make get request to google
-        events_url = API_BASE + "/events?access_token=#{@auth_token}"
-        response = Net::HTTP.get(URI(events_url))
-        JSON.parse(response)['items']
-        # wrap in class pulling out all google stuff
-            # look at items, make sure it's there!
-            # ANOTHER class handles the internals of items
-    end
-    
+  def events
+    #use http to make get request to google
+    events_url = API_BASE + "/events?access_token=#{@auth_token}"
+    response = Net::HTTP.get(URI(events_url))
+    JSON.parse(response)['items']
+    # wrap in class pulling out all google stuff
+        # look at items, make sure it's there!
+        # ANOTHER class handles the internals of items
+  end
+
 # Quick adds a calendar event
 # 
 # text          - the text sent to the Google Calendar API 
 #
 # Returns the JSON body of the response        
-    def quick_add(text)
-        res = post_api_request("/events/quickAdd", {'text' => text}, nil)
-        JSON.parse(res.body)
-    end
-    
+def quick_add(text)
+  res = post_api_request("/events/quickAdd", {'text' => text}, nil)
+  JSON.parse(res.body)
+end
+
 # Add a calendar event
 # 
 # start_time    - Ruby DateTime object, must be before end_time and have its time
@@ -90,21 +90,21 @@ class Calendar
 # description   - string, will be set as event summary (or 'title' to us normal people)
 #
 # Returns the JSON body of the response
-    def add(start_time, end_time, room, description)
-        params = {
-            "end" => {
-                "dateTime" => end_time.rfc3339
-            },
-            "start" => {
-                "dateTime" => start_time.rfc3339
-            },
-            "location" => room,
-            "summary" => description
-        }.to_json
-    
-        res = post_api_request("/events", nil, params)
-        JSON.parse(res.body)
-    end
+def add(start_time, end_time, room, description)
+  params = {
+    "end" => {
+      "dateTime" => end_time.rfc3339
+    },
+    "start" => {
+      "dateTime" => start_time.rfc3339
+    },
+    "location" => room,
+    "summary" => description
+  }.to_json
+
+  res = post_api_request("/events", nil, params)
+  JSON.parse(res.body)
+end
 
 # Check whether a proposed event overlaps temporally/spatially with any already scheduled events
 # 
@@ -116,30 +116,30 @@ class Calendar
 #
 # Returns true if proposed time/room combination can be scheduled, and
 #         false if it overlaps with already scheduled events
-    def bookable_for?(request_start, request_end, request_room)
-    
-        self.events.each do |event|
-            event_start = DateTime.rfc3339(event['start']['dateTime']) if event['start']['dateTime']
-            event_end = DateTime.rfc3339(event['end']['dateTime']) if event['end']['dateTime']
-            event_room = event['location']
-            
-            if event_start and event_end and event_room  # else something hasn't been specified for event, and it can't overlap?
-                                                            # or it's just a date -- do we care?
-                if event_room == request_room
-                    
-                    # not (end1 < start2 or end2 < start1) --- (http://c2.com/cgi/wiki?TestIfDateRangesOverlap)
-                    # http://makandracards.com/makandra/984-test-if-two-date-ranges-overlap-in-ruby-or-rails
-                    if ((request_start - event_end) * (event_start - request_end)) >= 0
-                        return false
-                    end
-                end    
-            end
-        end
-        return true
+def bookable_for?(request_start, request_end, request_room)
+
+    self.events.each do |event|
+      event_start = DateTime.rfc3339(event['start']['dateTime']) if event['start']['dateTime']
+      event_end = DateTime.rfc3339(event['end']['dateTime']) if event['end']['dateTime']
+      event_room = event['location']
+      
+      if event_start and event_end and event_room  # else something hasn't been specified for event, and it can't overlap?
+                                                    # or it's just a date -- do we care?
+        if event_room == request_room
+          
+          # not (end1 < start2 or end2 < start1) --- (http://c2.com/cgi/wiki?TestIfDateRangesOverlap)
+          # http://makandracards.com/makandra/984-test-if-two-date-ranges-overlap-in-ruby-or-rails
+          if ((request_start - event_end) * (event_start - request_end)) >= 0
+            return false
+          end
+        end    
+      end
     end
-    
-    private
-    
+    return true
+end
+
+private
+
 # Sends a post request containing either query parameters or a JSON request body
 # 
 # relative_url  - string indicating the URL relative to API_BASE
@@ -148,18 +148,18 @@ class Calendar
 # request_body  - JSON string containing request body
 #
 # Returns Net::HTTP response object
-        def post_api_request(relative_url, form_data, request_body)
-            uri = URI(API_BASE + relative_url)
-            req = Net::HTTP::Post.new(uri)
-            req.set_form_data(form_data) if form_data
-            if request_body
-                req.body = request_body
-                req['Content-Type'] = 'application/json'
-            end
-            req['Authorization'] = "Bearer #{@auth_token}"
-            res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-                http.request(req)
-            end
-            res
-        end
+  def post_api_request(relative_url, form_data, request_body)
+    uri = URI(API_BASE + relative_url)
+    req = Net::HTTP::Post.new(uri)
+    req.set_form_data(form_data) if form_data
+    if request_body
+      req.body = request_body
+      req['Content-Type'] = 'application/json'
+    end
+    req['Authorization'] = "Bearer #{@auth_token}"
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(req)
+    end
+    res
+  end
 end
