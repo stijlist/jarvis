d8e8d2968190e592106d5cdc42b8c2f76fc41ef3
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-08-16 22:52:55 -0400

    Fix bug in calendar initialization

--- jarvis.rb
+++ jarvis.rb
@@ -6,7 +6,7 @@ class Jarvis
   config = {greeting: 'Jarvis is online.',
             streams: File.read('./streams_filtered').lines.map(&:chomp)}
 
-  cal = Calendar.new(ENV['ZULIP_JARVIS_CALENDAR'])
+  cal = Calendar.new
   zulip = Zulip::Client.new do |config|
     config.email_address = ENV.fetch( 'ZULIP_JARVIS_EMAIL' )
     config.api_key = ENV.fetch( 'ZULIP_JARVIS_API_KEY' )
